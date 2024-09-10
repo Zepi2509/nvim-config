3d50c984fda1e634f8ced422570ca032cbdce676
Noah Zepner <noah@zepner.art>
2024-09-10 22:11:18 +0200

    update which-key

--- lua/zepi/plugins/wich-key.lua
+++ lua/zepi/plugins/wich-key.lua
@@ -1,16 +1,16 @@
 return {
-	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+  "folke/which-key.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  config = function() -- This is the function that runs, AFTER loading
+    require("which-key").setup()
 
-		-- Document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>f"] = { name = "[F]find", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-		})
-	end,
+    -- Document existing key chains
+    require("which-key").add({
+      { "<leader>c", group = "[C]ode" },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>f", group = "[F]ind" },
+      { "<leader>w", group = "[W]orkspace" },
+    })
+  end,
 }
